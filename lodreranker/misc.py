aefdfb2436b6e89cc8ddf97e281a6630f818db9d
Lorenzo Guidaldi <lorenzo.guidaldi@fhoster.com>
2019-10-21 18:52:55 +0200

    Fixed typo error in dbpedia movies query

--- lodreranker/misc.py
+++ lodreranker/misc.py
@@ -12,7 +12,7 @@ class Lod_queries:
                prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
                prefix dbpedia-owl: <http://dbpedia.org/ontology/>
                SELECT DISTINCT ?film ?abstract  WHERE  {
-                   ?film a dbpedia-owl:Filmw.
+                   ?film a dbpedia-owl:Film.
                    ?film rdfs:label ?label .
                    FILTER regex( str(?label),"""f'"{film}"'""", "i")
                    ?film dbpedia-owl:abstract ?abstract .
