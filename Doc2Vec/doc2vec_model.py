fcb748c9197141b961d88e7b041d0082387627b9
Marco <marco.bellocchi94@gmail.com>
2019-10-21 14:04:26 +0200

    Modelli aggiornati

--- Doc2Vec/doc2vec_model.py
+++ Doc2Vec/doc2vec_model.py
@@ -3,7 +3,7 @@ from gensim.models import Doc2Vec
 import os
 import gensim.models.doc2vec
 import multiprocessing
-from doc2vec_preprocessing import normalize_text, stopping
+from Doc2Vec.doc2vec_preprocessing import normalize_text, stopping
 
 
 # Per funzionare Doc2Vec accetta solamente TaggedDocument:
@@ -18,22 +18,21 @@ os.system("rm -r doc2vec_data_artists.model")
 os.system("rm -r data_artists_result.txt")
 
 cores = multiprocessing.cpu_count()
-
-f = open("data_artists.txt", encoding='utf-8')
-with open("data_artists_result.txt", 'a', encoding='utf-8') as file:
+f = open("data_artist.txt", encoding="utf8")
+with open("data_artist_result.txt", 'a', encoding="utf8") as file:
     for line in f:
         line = normalize_text(stopping(line))
         file.write(line + "\n")
 
-
-tagged_data = TaggedLineDocument("data_artists_result.txt")
+tagged_data = TaggedLineDocument("data_artist_result.txt")
 
 assert gensim.models.doc2vec.FAST_VERSION > -1, "This will be painfully slow otherwise"
-model = Doc2Vec(dm=1, vector_size=100, window=5, negative=5, hs=0, min_count=2, sample=0,
+model = Doc2Vec(dm=1, vector_size=100, window=5, hs=0, min_count=2, sample=0,
                 epochs=20, workers=cores)
 
 model.build_vocab(tagged_data)
 
 model.train(tagged_data, total_examples=model.corpus_count, epochs=model.epochs)
-model.save('doc2vec_data_artists.model')
+model.save('doc2vec_data_artist.model')
+
 
