1bd40e63b3783e18fef6dd365d1240b47b354ec7
Lorenzo Guidaldi <lorenzo.guidaldi@fhoster.com>
2019-10-23 23:04:32 +0200

    Refactor SPARQL wkd movie query (todo improve to avoid false positives)

--- lodreranker/misc.py
+++ lodreranker/misc.py
@@ -4,21 +4,23 @@ import urllib.request
 import requests
 from SPARQLWrapper import JSON, SPARQLWrapper
 
+
+# TODO improve query to avoid false positives
 def retrieveFilmAbstract(film):
-        sparql = SPARQLWrapper("https://query.wikidata.org/sparql", agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36")
-        query = """
-                SELECT DISTINCT ?item WHERE {
-                VALUES ?type """"{""wd:Q11424""}"""" ?item wdt:P31 ?type .
-                ?item rdfs:label ?queryByTitle.
-                ?item wdt:P1476 ?name.
-                FILTER(REGEX(?queryByTitle, """f'"{film}"'""", "i"))
-                }
-                LIMIT 1
-                """
-        sparql.setQuery(query)
-        sparql.setReturnFormat(JSON)
-        results = sparql.query().convert()
-        data = ""
-        for result in results["results"]["bindings"]:
-                data = (result["item"]["value"])
-        return data
+    sparql = SPARQLWrapper("https://query.wikidata.org/sparql", agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36")
+    query = """
+        SELECT DISTINCT ?item WHERE {
+        VALUES ?type """"{""wd:Q11424""}"""" ?item wdt:P31 ?type .
+        ?item rdfs:label ?queryByTitle.
+        ?item wdt:P1476 ?name.
+        FILTER(REGEX(?queryByTitle, """f'"{film}"'""", "i"))
+        }
+        LIMIT 1
+        """
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    bindings = sparql.query().convert()['results']['bindings']
+    if bindings:
+        return bindings[0]['item']['value']
+    else:
+        raise Exception(f'\tElement "{film}" not found')
