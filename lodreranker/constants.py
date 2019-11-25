d7df184fbfb9beffc6fef612eba5c54a22160344
Lorenzo Guidaldi <lorenzo.guidaldi@fhoster.com>
2019-11-25 19:47:44 +0100

    Query timeout increased because books geolocalized query may take 40 seconds

--- lodreranker/constants.py
+++ lodreranker/constants.py
@@ -5,7 +5,7 @@ MUSIC = 'artists'
 # limit the number of API calls to prevent saturating the app's rate limit
 FB_FETCH_PAGE_LIMIT = 5
 
-SPARQL_TIMEOUT = 30  # set the query timeout (in seconds)
+SPARQL_TIMEOUT = 60  # set the query timeout (in seconds)
 SPARQL_LIMIT_DEFAULT = 30  # TODO increase dynamically
 
 WIKIDATA = 'https://query.wikidata.org/sparql'
