abb2ab86d5d07ab77465bc19ce5d7f9793385847
Lorenzo Guidaldi <lorenzo.guidaldi@fhoster.com>
2019-11-27 00:47:19 +0100

    Add template filter with heuristic algorithm for restoring paragraphs given a string text

--- lodreranker/templatetags/template_extras.py
+++ lodreranker/templatetags/template_extras.py
@@ -1,4 +1,6 @@
 from django import template
+from lodreranker.utils import chunks
+import re
 
 register = template.Library()
 
@@ -8,3 +10,35 @@ def keyvalue(dict, key):
         return dict.get(key,'')
     except (KeyError, TypeError):
         return ''
+
+@register.filter
+def paragraphs(value):
+    output = ['<p>']
+    max_periods_per_paragraph = 2
+    periods_read = 0
+    words_read = 0
+    for word in value.split(' '):
+        output.append(word)
+        words_read+=1
+        if (word[-1:]=='.'):
+            if (words_read >= 15):
+                periods_read +=1
+                words_read = 0
+                if (periods_read % max_periods_per_paragraph == 0):
+                    output.append('</p><p>')
+    
+    output.append('</p>')
+    return ' '.join(output)
+
+@register.filter
+def extract(value):
+    return f"{value.split('</p>')[0]}</p>"
+
+
+@register.filter
+def percent(value):
+    if value<1:
+        return f'{round(value*100,2)}%'
+    else:
+        return value
+
